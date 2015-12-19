64145b45f24b9f85eac7e08ccf9cc2f2cccae5d0
Michael Palermo <mpalermo1@mail.usf.edu>
2015-12-19 03:56:43 -0500

    Create Yorick.java

---
+++ Yorick.java
@@ -0,0 +1,29 @@
+import java.util.*;
+
+public class Yorick
+{
+  public static void main (String[] args)
+  {
+    final String choices = "abcdefghijklmnopqrstuvwxyz ABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890!@#$%^&*()_+-=[]{};':,./<>?\"\\|`~";
+    final int chosen = choices.length();
+    final String quote = "Alas, poor Yorick! I knew him, Horatio";
+    
+    int test = 0;
+    String alas = "";
+
+
+    Random r = new Random(System.currentTimeMillis());
+    while (test == 0){
+      for (int i = 0; i < 39; i++){
+        alas += choices.charAt(r.nextInt(chosen));
+      }
+      System.out.println(alas);
+      if (alas.equals(quote)){
+        test = 1;
+      }
+      else{
+        alas = "";
+      }
+    }
+  }
+}
